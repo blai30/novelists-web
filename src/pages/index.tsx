e6573af10f06953c401523ad236bd5aa86ff6c51
Brian Lai <brianlai30@outlook.com>
2021-06-21 14:54:01 -0700

    setup project and dependencies

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="text-xl font-sans">
+      <button className="bg-green-300">
+        Tailwind
+      </button>
+      Hello world
+    </div>
+  )
+}
