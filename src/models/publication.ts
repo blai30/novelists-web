e3709767112569197ff70c0b64a080f98c2568b5
Brian Lai <brianlai30@outlook.com>
2021-07-06 23:57:57 -0700

    feat: add mock json database and initial browse page with api fetch

---
+++ src/models/publication.ts
@@ -0,0 +1,8 @@
+export interface Publication {
+  id: number
+  user_id: string
+  title: string
+  synopsis?: string
+  created_at?: string
+  updated_at?: string
+}
--- src/pages/browse/index.tsx
+++ src/pages/browse/index.tsx
@@ -1,10 +1,79 @@
 import { NextPage } from 'next'
 import { getLayout } from '@/components/shared/Layout'
+import { Publication } from '@/models/publication'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const Browse = () => {
+  const [publications, setPublications] = useState<Publication[]>([])
+
+  useEffect(() => {
+    const getPublications = async () => {
+      const response = await axios.get<Publication[]>('http://localhost:3001/publications')
+      setPublications(response.data)
+    }
+
+    getPublications()
+  }, [])
+
   return (
-    <div>
-      Browse
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Series
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Synopsis
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Chapters
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {publications.map((publication) => (
+                  <tr key={publication.id}>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="flex items-center">
+                        <div className="ml-4">
+                          <div className="text-sm font-medium text-gray-900">
+                            {publication.title}
+                          </div>
+                          <div className="text-sm text-gray-500">
+                            {publication.user_id}
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">
+                        {publication.synopsis}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      1
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
